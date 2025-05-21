56272484c1f6279ef44145c29a931fb0262387ec
Zahra Alvandi <zalvandi39@gmail.com>
2025-05-21 18:05:29 +0330

    update app.js

--- js/app.js
+++ js/app.js
@@ -4,16 +4,22 @@ const creatElem = document.querySelector(".create");
 const todosContainer = document.querySelector(".todos-container");
 const inputCard = document.querySelector(".input-card");
 const cancelElem = document.querySelector(".cancel");
+const closeModalX = document.querySelector(".close-modal-x");
+const inputElem = document.querySelector(".input")
 
 openModalBtn.addEventListener("click", function () {
     modalScreen.classList.remove("hidden");
+});
 
-    creatElem.addEventListener("click", function () {
-        let userTodoTitle = inputCard.value;
-        todosContainer.innerHTML = userTodoTitle;
-    });
+creatElem.addEventListener("click", function () {
+    let userTodoTitle = inputElem.value;
+    todosContainer.textContent = userTodoTitle;
+});
 
-    cancelElem.addEventListener("click", function () {
-        modalScreen.classList.add("hidden");
-    });
+cancelElem.addEventListener("click", function () {
+    modalScreen.classList.add("hidden");
+});
+
+closeModalX.addEventListener("click", function () {
+    modalScreen.classList.add("hidden");
 });
